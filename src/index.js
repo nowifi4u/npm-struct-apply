b698fd8cf9ed2f616d1a54f7f2129c2ade2f2254
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-08-21 02:08:20 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+function applyClassProp(classFrom, propFrom, classTo, propTo) {
+	Object.defineProperty(
+		classTo.prototype,
+		propTo,
+		Object.getOwnPropertyDescriptor(classFrom.prototype, propFrom)
+	);
+}
+
+function applyClassProps(classFrom, props = [], classTo, blacklist = false) {
+	for (const prop of props) {
+		if(props.includes(prop) === blacklist) continue;
+		applyClassProp(classFrom, prop, classTo, prop);
+	}
+}
+
+function applyPreCallProp(obj, prop, func) {
+	const oldFunc = obj[prop];
+	obj[prop] = function(...args) {
+		func.call(obj, ...args);
+		return oldFunc.call(obj, ...args);
+	}
+}
+
+function applyPostCallProp(obj, prop, func) {
+	const oldFunc = obj[prop];
+	obj[prop] = function(...args) {
+		const result = oldFunc.call(obj, ...args);
+		return func.call(obj, result, ...args);
+	}
+}
+
+function applyAsyncPreCallProp(pbj, prop, func) {
+	const oldFunc = obj[prop];
+	obj[prop] = async function(...args) {
+		await func.call(obj, ...args);
+		return await oldFunc.call(obj, ...args);
+	}
+}
+
+function applyAsyncPostCallProp(obj, prop, func) {
+	const oldFunc = obj[prop];
+	obj[prop] = async function(...args) {
+		const result = await oldFunc.call(obj, ...args);
+		return await func.call(obj, result, ...args);
+	}
+}
+
+module.exports = {
+	applyClassProp,
+	applyClassProps,
+	applyPreCallProp,
+	applyPostCallProp,
+	applyAsyncPreCallProp,
+	applyAsyncPostCallProp
+};
